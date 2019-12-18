b00e32c1ba0b556152b67ada0116a398b4fb6640
marcelogaldino <marcelinhomgo@gmail.com>
2019-12-18 07:21:55 -0300

    created menu active feature in order to mark the current path menu

--- public/script.js
+++ public/script.js
@@ -1,26 +1,8 @@
-// const modalOverlay = document.querySelector(".modal-overlay")
-// const cards = document.querySelectorAll(".card")
+const currentPage = location.pathname
+const links = document.querySelectorAll("header .links a")
 
-// for (let card of cards) {
-//     card.addEventListener("click", function() {
-//         modalOverlay.classList.add("active")
-//     })
-// }
-
-// document.querySelector(".close-modal").addEventListener("click", function() {
-//     modalOverlay.classList.remove("active")
-// })
-
-const instructors = document.querySelector("#instructors")
-const members = document.querySelector("#members")
-
-// console.log(members)
-
-document.querySelector("#members").addEventListener("click", function() {
-    document.querySelector("#members").classList.add("active")
-    console.log(members)
-})
-
-// members.addEventListener("click", function() {
-//     members.classList.add("active")
-// })
+for (let link of links) {
+    if (currentPage.includes(link.getAttribute("href"))){
+        link.classList.add("active")
+    }
+} 
